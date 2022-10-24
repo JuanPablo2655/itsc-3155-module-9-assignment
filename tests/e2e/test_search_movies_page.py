8838af9509c93e6b9cfc0ba5b231c8479f488127
JuanPablo2655 <personal@isidrocasiano.com>
2022-10-23 21:33:52 -0400

    test: unit and e2e

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,1 +1,14 @@
 # TODO: Feature 3
+from flask.testing import FlaskClient
+
+
+def test_search_movies_page(test_app: FlaskClient):
+    response = test_app.get('/movies/search?title=Star Wars')
+    assert b'Star Wars' in response.data
+    assert b'George Lucas' in response.data
+    assert b'4' in response.data
+
+
+def test_search_movies_page_not_found(test_app: FlaskClient):
+    response = test_app.get('/movies/search?title=Star Trek')
+    assert b'Star Trek movie not found' in response.data
