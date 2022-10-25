a1da456e70b4ce85bd4f1cffd6f23aa85567aaa1
Christian <acostacastanonchristian@gmail.com>
2022-10-25 17:14:24 -0400

    Resolved merge conflict

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,21 @@
 # TODO: Feature 3
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+movie_repository = get_movie_repository()
+movie_repository.create_movie('Star Wars', 'George Lucas', 4)
+
+
+def test_get_movie_by_title():
+    movie = movie_repository.get_movie_by_title('Star Wars')
+
+    assert type(movie) == Movie
+    assert movie.title == 'Star Wars'
+    assert movie.director == 'George Lucas'
+    assert movie.rating == 4
+
+
+def test_get_movie_by_title_not_found():
+    movie = movie_repository.get_movie_by_title('Star Trek')
+
+    assert movie is None
