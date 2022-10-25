ff5f9324522f7799f955f2eb091dfe3204a2a180
Christian <acostacastanonchristian@gmail.com>
2022-10-25 15:20:39 -0400

    e2e test update

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -3,11 +3,11 @@ from flask import Flask
 from flask.testing import FlaskClient
 
 def test_get_movies_no_data(test_app: FlaskClient): 
-    response = test_app.get('/movies/new')
+    response = test_app.get('/movies')
     assert b'<p class="mb-3">No Movies to display</p>' in response.data
 
 def test_get_movies(test_app: FlaskClient): 
-    response = test_app.get('/movies/new')
+    response = test_app.get('/movies')
     assert b'Groundhog day' in response.data
     assert b'Harold Ramis' in response.data
     assert b'5' in response.data
