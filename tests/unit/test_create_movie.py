77b39135698df404ceb8bed7dd7d62e8563d1a76
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2022-10-25 20:46:50 -0400

    Update test_create_movie.py

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,14 @@
 # TODO: Feature 2
+from src.models.movie import Movie
+from src.repositories.movie_repository import get_movie_repository
+
+movies = get_movie_repository()
+movies._db.__init__()
+
+def test_create_movie():
+    currentMovies = movies.get_all_movies()
+    movies._db.__init__()
+    get_movie_repository.create_movie('Mulholland Drive', 'David Lynch', 5)
+    assert len(currentMovies) == 1
+    get_movie_repository.create_movie('Wings', 'Larisa Shepitko', 5)
+    assert len(currentMovies) == 2
