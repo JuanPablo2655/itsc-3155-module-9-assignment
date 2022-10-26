367c4335b1c87cb4bd324d8aa914de70750704e7
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2022-10-25 21:16:02 -0400

    Update tests/unit/test_create_movie.py
    Co-authored-by: Isidro Casiano <isidroICP@outlook.com>

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -5,8 +5,8 @@ from src.repositories.movie_repository import get_movie_repository
 movies = get_movie_repository()
 
 def test_create_movie():
+    movies.__init__()
     currentMovies = movies.get_all_movies()
-    movies._db.__init__()
     get_movie_repository.create_movie('Mulholland Drive', 'David Lynch', 5)
     assert len(currentMovies) == 1
     get_movie_repository.create_movie('Wings', 'Larisa Shepitko', 5)
